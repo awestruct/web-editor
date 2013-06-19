a033d3901a9cfdcc2d791175bfff22448936f69a
LightGuard <lightguard.jp@gmail.com>
2013-06-19 12:35:28 -0600

    Initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebEditor::Application.config.secret_token = 'df601d4bbe5af5d9814d4782fc943dc6341a451294aac09142849e12f62a5ab3d7ed5ad74543356a68add99ba0e1c19ca81388f4a59f9be0cf3ebe4a7c4df527'
