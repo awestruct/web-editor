c5aea2d061054a00b7923d3d1a61ceec3cc95af9
LightGuard <lightguard.jp@gmail.com>
2013-06-29 04:20:26 -0600

    Setting up for next service

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,8 +3,9 @@ require_relative 'spec_helper'
 
 describe 'AwestructWebEditor::App' do
   context 'repo awestruct.org' do
-    specify "retreiving files" do
-      get '/repo/awestruct.org'
+    let(:base_method) {'/repo/awestruct.org'}
+    specify 'retrieving file list' do
+      get "#{base_method}"
       expect(last_response.status).to eq 200
 
       json_response = MultiJson.load last_response.body
@@ -13,6 +14,12 @@ describe 'AwestructWebEditor::App' do
       expect(json_response['Gemfile']['links']).to have_exactly(4).items
       expect(json_response['Gemfile']['links'][0]).to include 'text', 'url', 'method'
       expect(json_response['extensions']['children']['atomizer']['children']['description.md']['links'][0]['url']).to eq('http://example.org/repo/awestruct.org/extensions/atomizer/description.md')
+    end
+
+    specify 'retrieving file content' do
+      get "#{base_method}/extensions/atomizer/description.md"
+      expect(last_response.status).to eq 200
+      expect(last_response.header['Content-Type']).to eq 'text/plain;charset=utf-8'
     end
   end
 end
