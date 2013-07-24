deed6f43b39ced54f215c9f8f286497b79c92867
wesbos <wesbos@gmail.com>
2013-07-24 12:24:28 -0400

    removing old CSS file

--- assets/javascripts/directives/center.js
+++ assets/javascripts/directives/center.js
@@ -3,25 +3,27 @@ aw.directive('center',function($window){
   return {
     restrict : "A",
     link : function(scope,elem,attrs){
+      
       var resize = function() {
         var winHeight = $window.innerHeight - 90,
             overlayHeight = elem[0].offsetHeight,
             diff = (winHeight - overlayHeight) / 2;
             elem.css('top',diff+"px");
-            console.log(winHeight,overlayHeight,diff);
       };
-      resize();
+
+      var watchForChange = function() {
+        return scope.$parent.data.overlaytype;
+      }
+      scope.$watch(watchForChange,function() {
+        $window.setTimeout(function() {
+          resize();
+        }, 1);
+      })
 
       angular.element($window).bind('resize',function(e){
-        // console.log('resafsdf');
-        // console.log(elem.css('top'));
+        console.log(scope.$parent.data.overlaytype)
         resize();
       });
-      // elem.bind('mouseenter',function(e){
-      //   scope.data.gridHoverPoints.x = attrs.x;
-      //   scope.data.gridHoverPoints.y = attrs.y;
-      //   scope.$apply();
-      // });
     }
   };
 });
