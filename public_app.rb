c936eb6c9ea1f5882a6ead98d3af1f8a9081bc43
wesbos <wesbos@gmail.com>
2013-08-28 16:32:30 -0400

    tokens implemented

--- public_app.rb
+++ public_app.rb
@@ -205,9 +205,6 @@ module AwestructWebEditor
       include Sinatra::Cookies
 
       def check_token(token)
-        puts "===================Server side token:========================================"
-        puts session[:csrf]
-        puts "==========================================================="
         unless env['HTTP_TOKEN'] == (Digest::SHA512.new << "#{session[:csrf]}#{env['HTTP_TIME']}").to_s
           error 401, 'You are not allowed to do this.'
         end
